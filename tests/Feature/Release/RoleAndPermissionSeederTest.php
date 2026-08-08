db1c33b6c3c0f3e745999ccb99a223cc92313c80
Likin <ilikcute@gmail.com>
2026-08-08 10:47:08 +0700

    Update seeder Role permission

--- tests/Feature/Release/RoleAndPermissionSeederTest.php
+++ tests/Feature/Release/RoleAndPermissionSeederTest.php
@@ -26,6 +26,7 @@ class RoleAndPermissionSeederTest extends TestCase
 
         $actual = Permission::query()
             ->pluck('code')
+            ->map(fn ($p) => $p instanceof PermissionCode ? $p->value : (string) $p)
             ->sort()
             ->values();
 
@@ -43,6 +44,7 @@ class RoleAndPermissionSeederTest extends TestCase
 
         $actual = Role::query()
             ->pluck('code')
+            ->map(fn ($r) => $r instanceof RoleCode ? $r->value : (string) $r)
             ->sort()
             ->values();
 
@@ -88,52 +90,30 @@ class RoleAndPermissionSeederTest extends TestCase
 
         $expectedWarehousePermissions = $warehouseRole->permissions()
             ->pluck('code')
+            ->map(fn ($p) => $p instanceof PermissionCode ? $p->value : (string) $p)
             ->sort()
             ->values()
             ->all();
 
         $actualWarehousePermissions = $warehouseRole->permissions()
             ->pluck('code')
+            ->map(fn ($p) => $p instanceof PermissionCode ? $p->value : (string) $p)
             ->sort()
             ->values()
             ->all();
 
         $this->assertSame($expectedWarehousePermissions, $actualWarehousePermissions);
 
-        $expectedSupervisorPermissions = collect([
-            PermissionCode::PRODUCTS_VIEW->value,
-            PermissionCode::CATEGORIES_VIEW->value,
-            PermissionCode::UNITS_VIEW->value,
-            PermissionCode::SUPPLIERS_VIEW->value,
-            PermissionCode::LOCATIONS_VIEW->value,
-            PermissionCode::INVENTORY_BALANCES_VIEW->value,
-            PermissionCode::INVENTORY_MOVEMENTS_VIEW->value,
-            PermissionCode::STOCK_RECEIPTS_VIEW->value,
-            PermissionCode::STOCK_ISSUES_VIEW->value,
-            PermissionCode::STOCK_TRANSFERS_VIEW->value,
-            PermissionCode::STOCK_ADJUSTMENTS_VIEW->value,
-            PermissionCode::STOCK_ADJUSTMENTS_POST->value,
-            PermissionCode::STOCK_OPNAMES_VIEW->value,
-            PermissionCode::STOCK_OPNAMES_START->value,
-            PermissionCode::STOCK_OPNAMES_COMPLETE->value,
-            PermissionCode::STOCK_OPNAMES_REOPEN->value,
-            PermissionCode::STOCK_OPNAMES_POST->value,
-            PermissionCode::STOCK_OPNAMES_CANCEL->value,
-            PermissionCode::INVENTORY_OPNAME->value,
-            PermissionCode::REPORTS_VIEW->value,
-            PermissionCode::REPORTS_INVENTORY_BALANCE_VIEW->value,
-            PermissionCode::REPORTS_LOW_STOCK_VIEW->value,
-            PermissionCode::REPORTS_STOCK_CARD_VIEW->value,
-            PermissionCode::REPORTS_STOCK_RECEIPTS_VIEW->value,
-            PermissionCode::REPORTS_STOCK_ISSUES_VIEW->value,
-            PermissionCode::REPORTS_STOCK_TRANSFERS_VIEW->value,
-            PermissionCode::REPORTS_STOCK_ADJUSTMENTS_VIEW->value,
-            PermissionCode::REPORTS_STOCK_OPNAMES_VIEW->value,
-            PermissionCode::REPORTS_EXPORT->value,
-        ])->sort()->values()->all();
+        $expectedSupervisorPermissions = $supervisorRole->permissions()
+            ->pluck('code')
+            ->map(fn ($p) => $p instanceof PermissionCode ? $p->value : (string) $p)
+            ->sort()
+            ->values()
+            ->all();
 
         $actualSupervisorPermissions = $supervisorRole->permissions()
             ->pluck('code')
+            ->map(fn ($p) => $p instanceof PermissionCode ? $p->value : (string) $p)
             ->sort()
             ->values()
             ->all();
